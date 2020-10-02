be8b8d7bd09cf4c01a36dd392a1ab7681274dbb9
Isaac <isaacagutierrez10@yahoo.com>
2020-10-02 00:13:20 -0500

    Bound initial data to ListView

---
+++ Item.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem
+{
+    class Item
+    {
+        private int stock;
+        private double price;
+        private string location;
+
+        public Item(int stock, double price, string location)
+        {
+            this.stock = stock;
+            this.price = price;
+            this.location = location;
+        }
+
+        public int Stock
+        {
+            get
+            {
+                return this.stock;
+            }
+            set
+            {
+                if(stock != value)
+                {
+                    stock = value;
+                }
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                if(price != value)
+                {
+                    price = value;
+                }
+            }
+        }
+        public string Location
+        {
+            get
+            {
+                return this.location;
+            }
+            set
+            {
+                if(location != value)
+                {
+                    location = value;
+                }
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,20 @@ namespace InventorySystem
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+        
+      
+
         public MainWindow()
         {
             InitializeComponent();
+            ObservableCollection<Item> items = new ObservableCollection<Item>();
+            items.Add(new Item(50, 23.3, "abc123"));
+            items.Add(new Item(48, 55.3, "cba123"));
+            items.Add(new Item(27, 22.8, "abc123"));
+            this.DataContext = items;
+            
         }
+
     }
 }
